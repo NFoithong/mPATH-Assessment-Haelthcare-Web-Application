7b6fd835404dbb2fb257d1848c23d419befea293
NFoithong <n.foithong1983@gmail.com>
2025-02-01 00:43:13 -0500

    Configure Authentication (Program.cs) Modify Program.cs

--- HealthcareApp/Program.cs
+++ HealthcareApp/Program.cs
@@ -1,13 +1,48 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using HealthcareAPI.Data;
+using HealthcareAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+// Configure Database
 builder.Services.AddDbContext<HealthcareContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configure Identity
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<HealthcareContext>()
+    .AddDefaultTokenProviders();
+
+// Configure Authentication & JWT
+var key = Encoding.UTF8.GetBytes("SuperSecretKey@123");  // Secure this!
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "Bearer";
+    options.DefaultChallengeScheme = "Bearer";
+})
+.AddJwtBearer("Bearer", options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+builder.Services.AddControllers();
+
+
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
@@ -21,6 +56,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 app.MapControllers();
