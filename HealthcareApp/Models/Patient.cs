1a753f6514ab2eba4ed8653ed1ff1dca6d96024c
NFoithong <n.foithong1983@gmail.com>
2025-01-31 23:03:02 -0500

    Create models folder with Patient and Recommendation files

---
+++ HealthcareApp/Models/Patient.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthcareAPI.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int PatientId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Gender { get; set; }
+
+        public List<Recommendation> Recommendations { get; set; } = new();
+    }
+}
---
+++ HealthcareApp/Models/Recommendation.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthcareAPI.Models
+{
+    public class Recommendation
+    {
+        [Key]
+        public int RecommendationId { get; set; }
+
+        [Required]
+        public string Type { get; set; } // Allergy Check, Screening
+
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+
+        public int PatientId { get; set; }
+
+        public Patient Patient { get; set; }
+    }
+}
