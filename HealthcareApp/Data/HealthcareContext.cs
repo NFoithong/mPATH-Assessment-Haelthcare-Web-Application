3d7f67a2fcc3096ce3a77b91323c4261151b0989
NFoithong <n.foithong1983@gmail.com>
2025-02-01 00:40:45 -0500

    Update HealthcareContext.cs to Configure Identity & Authentication

--- HealthcareApp/Data/HealthcareContext.cs
+++ HealthcareApp/Data/HealthcareContext.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using HealthcareAPI.Models;
 
 namespace HealthcareAPI.Data
 {
-    public class HealthcareContext : DbContext
+    public class HealthcareContext : IdentityDbContext<User>
     {
         public HealthcareContext(DbContextOptions<HealthcareContext> options) : base(options) { }
 
