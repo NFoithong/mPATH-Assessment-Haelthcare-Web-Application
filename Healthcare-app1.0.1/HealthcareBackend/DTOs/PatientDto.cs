1a09a11a712b99fb2baa11b7d491d546b46477c8
NFoithong <n.foithong1983@gmail.com>
2025-02-02 14:17:34 -0500

    add DTOs (Data Transfer Objects) ensure data integrity and security.

---
+++ Healthcare-app1.0.1/HealthcareBackend/DTOs/PatientDto.cs
@@ -0,0 +1,9 @@
+namespace HealthBackend.DTOs
+{
+    public class PatientDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MedicalRecordNumber { get; set; }
+    }
+}
